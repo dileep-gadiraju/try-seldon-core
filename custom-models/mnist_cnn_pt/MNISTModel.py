0d106927f9a2dc3a43b891499fd907d4d06b555c
Dileep Gadiraju <dileep.gadiraju@tarento.com>
2022-12-12 13:31:02 +0530

    custom model changes

--- custom-models/mnist_cnn_pt/MNISTModel.py
+++ custom-models/mnist_cnn_pt/MNISTModel.py
@@ -1,31 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-    
+
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10
+# Build a feed-forward network
+
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout2d(0.25)
-        self.dropout2 = nn.Dropout2d(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.liner1=nn.Linear(input_size, hidden_sizes[0])
+        self.liner2=nn.Linear(hidden_sizes[0], hidden_sizes[1])
+        self.liner3=nn.Linear(hidden_sizes[1], output_size)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.conv2(x)
-        x = F.relu(x)
-        x = F.max_pool2d(x, 2)
-        x = self.dropout1(x)
-        x = torch.flatten(x, 1)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.dropout2(x)
-        x = self.fc2(x)
-        output = F.softmax(x, dim=1)
-        return output
+        model = nn.Sequential(self.liner1,
+                      nn.ReLU(),
+                      self.liner2,
+                      nn.ReLU(),
+                      self.liner3,
+                      nn.LogSoftmax(dim=1))
+        return model
 
 class MNISTModel:
     def __init__(self):
--- custom-models/mnist_cnn_pt/README.md
+++ custom-models/mnist_cnn_pt/README.md
@@ -1,7 +1,8 @@
 #### Custom Model built with PyTorch and MNIST dataset to predict handwritten digits. Packaged as docker image with Seldon to expose as microservice model as microservice.
 
 Temp Reference: https://docs.primehub.io/docs/model-deployment-language-wrapper-python#pytorch
-
+#### Setup MINIO
+1) [MINIO Setup](../../minio_setup.ipynb)
 #### Style 1 : Package as Docker image with seldon-core-microservice
 1) Build Image
    
@@ -9,9 +10,9 @@ Temp Reference: https://docs.primehub.io/docs/model-deployment-language-wrapper-
 
 2) Test the Image
     
-    `docker run -p 9000:9000 --rm mnist-cnn-pt:latest`
+    `docker run -p 9001:9001 --rm mnist-cnn-pt:latest`
 
-    `curl -X POST http://localhost:9000/api/v1.0/predictions \
+    `curl -X POST http://localhost:9001/api/v1.0/predictions \
     -H 'Content-Type: application/json' \
     -d '{ "data": { "ndarray": [[5.964,4.006,2.081,1.031]]}}'`
 
